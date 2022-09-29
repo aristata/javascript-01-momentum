092e4b91fe97180988c04fbc70e30c5c069c10cd
장성민 <aristatait@gmail.com>
2022-09-29 17:40:41 +0900

    feat: boolean and null and undefined

--- app.js
+++ app.js
@@ -6,3 +6,33 @@ const b = 2;
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+
+let nickName = "aris";
+
+console.log("My nick name is ", nickName);
+
+nickName = "earnestgem";
+
+console.log("My nick name is ", nickName);
+
+/*
+  `boolean`
+  - `true` or `false` 값을 가지는 타입
+
+  `null`
+  - 아무것도 없는 상태
+  - 아무것도 없다는 값이 들어 있음
+  - 변수 안에 어떤 것이 없다는 것을 확실히 하기 위해 사용함
+
+  `undefined` 
+  - 선언되었지만, 정의되지 않음
+  - 컴퓨터 메모리 안에는 존재하지만 (=공간은 있지만), 값이 없는 상태
+*/
+const amIFat = true;
+console.log("amIFat 의 타입은?", typeof amIFat);
+
+const nothing = null;
+console.log("nothing 의 값은?", nothing);
+
+let something;
+console.log("something 의 값은?", something);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# 이 프로젝트는 무엇인가요?
+
+Nomad coders 의 자바스크립트 강의
+
+> [바닐라 JS 로 크롬 앱 만들기](https://nomadcoders.co/javascript-for-beginners/lobby)
+
+를 수강하고, 해당 내용을 활용하여 만든 프로젝트 입니다
