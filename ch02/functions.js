8329006d003de20776cfc2befe5d7d747ca16d5e
장성민 <aristatait@gmail.com>
2022-09-30 10:22:32 +0900

    feat: functions with param

--- ch02/functions.js
+++ ch02/functions.js
@@ -1,9 +1,27 @@
-function sayHello() {
-  console.log("Hello");
+function sayHello(name, age) {
+  console.log("Hello " + name + "(" + age + ")");
 }
+sayHello("aris", 12);
+sayHello("ban", 54);
+sayHello("catt", 91);
+sayHello("dam", 38);
+sayHello("film", 22);
 
-sayHello();
-sayHello();
-sayHello();
-sayHello();
-sayHello();
+function plus(a, b) {
+  return a + b;
+}
+console.log(plus(3, 25));
+
+function multi(a, b) {
+  console.log(a * b);
+}
+multi(4, 102);
+
+const player = {
+  name: "aris",
+  sayHi: function (otherPlayerName) {
+    console.log(`Hi ${otherPlayerName}, nice to meet you`);
+  }
+};
+player.sayHi("miho");
+player.sayHi("minho");
