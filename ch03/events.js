3755b25ee74dbe4ed2251117c1476cb57c17914f
earnestgem <aristatait@gmail.com>
2022-10-02 21:45:32 +0900

    feat: more events

--- ch03/events.js
+++ ch03/events.js
@@ -11,7 +11,7 @@ function handleTitleClick() {
 
 // 자바스크립트는 누군가가 title 을 click 하는 것을 listen(지켜보다) 할 것이다
 // click 이벤트가 발생하면 두번째 인자로 받은 handleTitleClick 함수가 실행될 것이다
-title.addEventListener("click", handleTitleClick);
+// title.addEventListener("click", handleTitleClick);
 
 // 마우스 동작을 감지하였을 때 실행될 함수
 function handleMouseEnter() {
@@ -29,3 +29,16 @@ function handleMouseLeave() {
 
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+// 화면 사이즈 수정 이벤트
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+window.addEventListener("resize", handleWindowResize);
+
+// copy 이벤트
+function handleCopy(e) {
+  alert("copied!!!");
+  console.log(e.target.textContent);
+}
+window.addEventListener("copy", handleCopy);
