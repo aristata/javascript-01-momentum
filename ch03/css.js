d452cd11a5ce9e9f41dcf02573a6eca99d8bcfd1
earnestgem <aristatait@gmail.com>
2022-10-03 20:42:03 +0900

    feat: css in javascript

---
+++ ch03/css.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector("div.hello2 h1:first-child");
+
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+
+h1.addEventListener("click", handleTitleClick);
