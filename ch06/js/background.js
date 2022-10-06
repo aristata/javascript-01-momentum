65fe93e45d134d71cccb265e39d66045a619da1a
장성민 <aristatait@gmail.com>
2022-10-06 15:33:53 +0900

    refactor: img 경로 변경

--- ch06/js/background.js
+++ ch06/js/background.js
@@ -12,6 +12,6 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-bgImage.src = `./img/${chosenImage}`;
+bgImage.src = `../img/${chosenImage}`;
 bgImage.className = "backgroundImg";
 document.body.appendChild(bgImage);
