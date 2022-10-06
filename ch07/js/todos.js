d1654709a84a4cd4d5406bc06d9712266e16ce1d
장성민 <aristatait@gmail.com>
2022-10-06 17:03:15 +0900

    feat: 로컬스토리지에 todo 저장

--- ch07/js/todos.js
+++ ch07/js/todos.js
@@ -2,32 +2,30 @@ const todosFormElement = document.querySelector("#todos");
 const todosInputElement = todosFormElement.querySelector("input");
 const todoListElement = document.querySelector("#todoList");
 
-// todo 를 화면에서 삭제하는 함수
+// todo가 저장될 배열
+const todos = [];
+
+// 로컬스토리지에 todos 를 저장하는 함수
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function deleteTodo(event) {
-  // event 객체에서 부모 li 요소를 찾는다
   const parentLi = event.target.parentNode;
 
-  // 부모 li 를 지운다
   parentLi.remove();
 }
 
-// todo 를 화면에 그리는 함수
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   span.innerText = newTodo;
 
-  // button 요소 생성
   const button = document.createElement("button");
-
-  // button 값 추가
   button.innerText = "❌";
-
-  // button 이벤트 추가
   button.addEventListener("click", deleteTodo);
 
   li.appendChild(span);
-  // li에 button 추가, 위치는 span 다음
   li.appendChild(button);
   todoListElement.appendChild(li);
 }
@@ -35,10 +33,12 @@ function paintTodo(newTodo) {
 function todoSubmitHandler(event) {
   event.preventDefault();
   const newTodo = todosInputElement.value;
-  // console.log(`newTodo => ${newTodo}`);
   todosInputElement.value = "";
-  // todo 를 화면에 그리는 함수를 호출
   paintTodo(newTodo);
+  // newTodo 를 todos 배열에 추가
+  todos.push(newTodo);
+  // 로컬스토리지에 저장하는 함수를 호출
+  saveTodos();
 }
 
 todosFormElement.addEventListener("submit", todoSubmitHandler);
