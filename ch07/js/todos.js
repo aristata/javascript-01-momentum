3c502fdcb0cf5b39e1ef0b6593d33b5998b97356
장성민 <aristatait@gmail.com>
2022-10-06 16:38:13 +0900

    feat: delete todo

--- ch07/js/todos.js
+++ ch07/js/todos.js
@@ -1,31 +1,41 @@
 const todosFormElement = document.querySelector("#todos");
 const todosInputElement = todosFormElement.querySelector("input");
-
-// todo list 요소를 찾아 변수에 저장한다
 const todoListElement = document.querySelector("#todoList");
+
+// todo 를 화면에서 삭제하는 함수
+function deleteTodo(event) {
+  // event 객체에서 부모 li 요소를 찾는다
+  const parentLi = event.target.parentNode;
+
+  // 부모 li 를 지운다
+  parentLi.remove();
+}
 
 // todo 를 화면에 그리는 함수
 function paintTodo(newTodo) {
-  // li 요소 생성
   const li = document.createElement("li");
-
-  // span 요소 생성
   const span = document.createElement("span");
-
-  // span 을 li 의 자식으로 붙임
-  li.appendChild(span);
-
-  // span 에 값 추가
   span.innerText = newTodo;
 
-  // li 를 todoList(ul) 의 자식으로 붙임
+  // button 요소 생성
+  const button = document.createElement("button");
+
+  // button 값 추가
+  button.innerText = "❌";
+
+  // button 이벤트 추가
+  button.addEventListener("click", deleteTodo);
+
+  li.appendChild(span);
+  // li에 button 추가, 위치는 span 다음
+  li.appendChild(button);
   todoListElement.appendChild(li);
 }
 
 function todoSubmitHandler(event) {
   event.preventDefault();
   const newTodo = todosInputElement.value;
-  console.log(`newTodo => ${newTodo}`);
+  // console.log(`newTodo => ${newTodo}`);
   todosInputElement.value = "";
   // todo 를 화면에 그리는 함수를 호출
   paintTodo(newTodo);
