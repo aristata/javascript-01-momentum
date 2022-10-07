040cee0231ec0a467b4307c794c831f0d8d99a0b
장성민 <aristatait@gmail.com>
2022-10-07 14:29:12 +0900

    feat: 날씨

--- ch08/js/weather.js
+++ ch08/js/weather.js
@@ -1,7 +1,21 @@
+const API_KEY = "1dd68e5b90437da0511e6beaeb9dbeaa";
+const locationElement = document.querySelector("#location");
+const weatherElement = document.querySelector("#weather");
+const temperatureElement = document.querySelector("#temperature");
+
 function onGeoOk(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   console.log(`현재 당신의 위치는 ${latitude}, ${longitude} 입니다`);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      locationElement.innerText = data.name;
+      weatherElement.innerText = data.weather[0].main;
+      temperatureElement.innerText = data.main.temp;
+    });
 }
 
 function onGeoError() {
